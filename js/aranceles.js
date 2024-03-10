b5fbf757df9099671c9d76b725d26e83b8faccd1
--global <--global>
2024-03-10 17:01:13 -0300

    Se crea primer archivo en javascript

---
+++ js/aranceles.js
@@ -0,0 +1,35 @@
+const let precioSuper= 871;
+const let ltsPrimera= 14;
+const let ltsSegunda= 14;
+const let ltsSexta= 12;
+const let ltsSeptima= 10;
+const let ltsCaballeros= 15;
+let categoria;
+//Calculo de aranceles por arbitro
+let primera = 'NO';
+let segunda = 'NO';
+let sexta = 'NO';
+let septima = 'NO';
+let caballeros = 'NO';
+
+alert("Bienvenido colega! Aca podrás calcular tus aranceles para este fin de semana");
+primera = text.toUpperCase(prompt("Tenes que arbitrar Primera división este finde semana? (SI/NO)"));
+if(primera = 'SI'){
+    let cantPrimera=parseInt(prompt('Cuántos partidos (Primera división)?'));
+}
+segunda = text.toUpperCase(prompt("Tenes que arbitrar Segunda división este finde semana? (SI/NO)"));
+if(segunda = 'SI'){
+    let cantSegunda=parseInt(prompt('Cuántos partidos (Segunda división)?'));
+}
+sexta = text.toUpperCase(prompt("Tenes que arbitrar Sexta división este finde semana? (SI/NO)"));
+if(sexta = 'SI'){
+    let cantSexta=parseInt(prompt('Cuántos partidos (Sexta división)?'));
+}
+septima = text.toUpperCase(prompt("Tenes que arbitrar Septima división este finde semana? (SI/NO)"));
+if(septima = 'SI'){
+    let cantSeptima=parseInt(prompt('Cuántos partidos (Septima división)?'));
+}
+caballeros = text.toUpperCase(prompt("Tenes que arbitrar Caballeros este finde semana? (SI/NO)"));
+if(caballeros = 'SI'){
+    let cantCaballeros=parseInt(prompt('Cuántos partidos (Caballeros)?'));
+}
